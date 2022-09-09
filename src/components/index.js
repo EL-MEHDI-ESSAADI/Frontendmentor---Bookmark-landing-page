82c318d24d8c4267e7d00fbf5c74238207266f96
El Mehdi Essaadi <gsio1487@gmail.com>
2022-09-09 22:57:02 +0100

    finish FAQs section

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Logo } from "./logo";
 export { default as Home } from "./home";
 export { default as FeaturesSection } from "./featuresSection";
 export { default as DownloadSection } from "./downloadSection";
+export { default as FAQsSection } from "./fAQsSection";
+export { default as Accordion } from "./accordion";
